7fb9cd41ac2029d2a15eed37ba6be327ef55d443
René Rivas <reinaldorrivas@gmail.com>
2025-12-15 20:41:29 -0400

    fh-moden-js: Reduced unnecessary reasignations

--- src/todos/use-cases/todo-builder/todoBuilder.js
+++ src/todos/use-cases/todo-builder/todoBuilder.js
@@ -1,13 +1,22 @@
 import { buildTodoHTML } from "../";
 
+let element;
+
 /**
  *
  * @param {string} elementName
  * @param {Todo} todo
  */
 export const todoBuilder = (elementName, todos = []) => {
-  // TODO: Referencia
-  const element = document.body.querySelector(elementName);
+  if (!element) {
+    element = document.body.querySelector(elementName);
+  }
+
+  if (!element) {
+    throw new Error(`Element ${elementName} not found`);
+  }
+
+  element.innerHTML = "";
 
   todos.forEach((todo) => {
     element.append(buildTodoHTML(todo));
